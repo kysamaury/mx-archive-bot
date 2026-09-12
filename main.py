9bc46403cbe3f7f102484320ef53fbd49dfa293a
amaury <kysamaury@gmail.com>
2026-09-12 15:59:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def on_member_join(member: discord.Member):
 
 #goodbye system or shii
 
-GOODBYE_CHANNEL_ID = 1234567890123456789 
+GOODBYE_CHANNEL_ID = 1538562192952266783
 
 @bot.event
 async def on_member_remove(member):
